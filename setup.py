4ec60db5c70b2ae9b503229f19362092722ca314
Brandon Amos <brandon.amos.cs@gmail.com>
2023-04-26 11:32:07 -0400

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="cvxpylayers",
-    version="0.1.6",
+    version="0.1.7",
     description="Differentiable convex optimization layers",
     long_description=long_description,
     long_description_content_type="text/markdown",
