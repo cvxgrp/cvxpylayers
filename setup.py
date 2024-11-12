ca9da8a82d3437b9830b57cca1d70230ebe4b396
Parth Nobel <ptnobel@stanford.edu>
2024-11-12 15:23:50 -0800

    Releases 0.1.8

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="cvxpylayers",
-    version="0.1.7",
+    version="0.1.8",
     description="Differentiable convex optimization layers",
     long_description=long_description,
     long_description_content_type="text/markdown",
