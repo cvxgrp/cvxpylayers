986f0cd1bfb2b0a3db47933dee63fa95f5e68e93
Parth Nobel <ptnobel@stanford.edu>
2024-12-05 17:33:09 -0800

    Bumps version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="cvxpylayers",
-    version="0.1.8",
+    version="0.1.9",
     description="Differentiable convex optimization layers",
     long_description=long_description,
     long_description_content_type="text/markdown",
